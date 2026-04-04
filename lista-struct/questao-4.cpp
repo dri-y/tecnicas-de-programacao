f269e5ea7123df3e8dce4a3c810ae7be3f57ec34
dri-y <adriandiluka@gmail.com>
2026-04-03 21:41:16 -0300

    Define 'Pessoa' struct and include necessary headers
    Added includes and defined 'Pessoa' struct with attributes.

--- lista-struct/questao-4.cpp
+++ lista-struct/questao-4.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+
+  struct Pessoa{
+      string nome;
+      int idade;
+      float altura;
+  };
+float SomaAlturas (Pessoa vetor[], int tamanho){
+  
