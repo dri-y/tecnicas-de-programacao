ba187af8a265e0e87c7c2e960016ebb91016ff3e
dri-y <adriandiluka@gmail.com>
2026-04-11 19:56:39 -0300

    Atualizar o questao-1.cpp

--- lista-vector/questao-1.cpp
+++ lista-vector/questao-1.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+double calcularMedia(vector<double> leituras_temp) {
+    double acumulador = 0.0;
+
+    
+    for (double t : leituras_temp) {
+        acumulador += t;
+    }
+
+    
+    if (leituras_temp.empty()) {
+        return 0.0;
+    }
+
+   
+    return acumulador / leituras_temp.size();
+}
+
+int main() {
+    
+    vector<double> leituras = {25.0, 23.0, 30.0};
+
+    
+    double media = calcularMedia(leituras);
+
+    
+    cout << "Media das leituras: " << media << endl;
+
+    return 0;
 
